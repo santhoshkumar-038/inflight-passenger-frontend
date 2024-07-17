5853b2fc1f5ba3f6fc4075ddd2ff53412f51c9dd
santhoshkumar-038 <64962939+santhoshkumar-038@users.noreply.github.com>
2024-07-17 12:49:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PlanList from './components/PlanList';
+
+function App() {
+  return (
+    <div className="App">
+      <PlanList />
+    </div>
+  );
+}
+
+export default App;
